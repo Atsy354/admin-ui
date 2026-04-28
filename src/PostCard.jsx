91e633afa8b0cef18d423fceaaf9d37fa3af13e8
Atsy354 <aksiomayufuna@gmail.com>
2026-04-28 08:35:22 +0700

    update uts

--- src/PostCard.jsx
+++ src/PostCard.jsx
@@ -5,23 +5,19 @@ function PostCard({ title, body }) {
 
   return (
     <div
-      className="h-64 flex flex-col justify-between bg-gray-100 p-5 rounded-lg 
-                 border border-gray-300 shadow-sm
-                 transition-all duration-300
-                 hover:shadow-md hover:border-black hover:bg-gray-200"
-    >
-      
-      {/* TITLE */}
+  className="h-64 flex flex-col justify-between bg-gray-100 p-5 rounded-lg 
+             border border-gray-300 shadow-sm
+             transition-all duration-300 ease-in-out
+             hover:shadow-lg hover:scale-[1.02] hover:bg-red-100 hover:border-black cursor-pointer"
+>
       <h2 className="text-lg font-semibold text-center text-gray-800 line-clamp-2">
         {title}
       </h2>
 
-      {/* BODY */}
       <p className="text-base text-center text-gray-600 line-clamp-3 mt-2">
         {body}
       </p>
 
-      {/* BUTTON */}
       <button
         onClick={() => setClicked(true)}
         className="w-full py-2 text-sm rounded-md text-white transition-all duration-300"
@@ -32,7 +28,7 @@ function PostCard({ title, body }) {
           if (clicked) e.currentTarget.style.backgroundColor = "#c92f18";
         }}
         onMouseOut={(e) => {
-          if (clicked) e.currentTarget.style.backgroundColor = "#ea3a1f";
+          if (clicked) e.currentTarget.style.backgroundColor = "#b83016";
         }}
       >
         {clicked ? "Tombol sudah diklik" : "Silakan Klik"}
